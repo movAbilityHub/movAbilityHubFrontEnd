8596cb1b0dec96a0e2b55bc2e6a74de5dab6307d
SkywalkerZ <abraarshk98@gmail.com>
2019-08-08 19:30:54 +0530

    #1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/TravelAgent/Dashobard" component={TravelAgent} />
+        <Route path="/TravelAgent/Dashboard" component={TravelAgent} />
       </Router>
     );
   }
