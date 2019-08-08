8f5b8a948aa70ef5ebfda32fac22da789a3349cd
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-08 19:28:23 +0530

    path to home added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import TravelAgent from "./components/travelAgent/dashboard";
+import Home from "./components/home/home";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Route path="/TravelAgent/Dashobard" component={TravelAgent} />
+        <Route path="/" component={Home} />
       </Router>
     );
   }
