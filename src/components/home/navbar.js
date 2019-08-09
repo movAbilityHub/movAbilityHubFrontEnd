6f6a515ace042f5a3ad1952254828aafb11ffe44
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-09 20:13:41 +0530

    nav bar formatted

--- src/components/home/navbar.js
+++ src/components/home/navbar.js
@@ -22,17 +22,28 @@ class NavigationBar extends Component {
           <b className="ability">Ability</b> Hub{" "}
           <sub className="smallFont">by IATA</sub>
         </Navbar.Brand>
-        <div className="placeHolder">
-        </div>
+        <div className="placeHolder" />
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav.Link className="active" href="/IATA">IATA</Nav.Link>
-            <Nav.Link className="active" href="/Airline">Airline</Nav.Link>
-            <Nav.Link className="active" href="/Airport">Airport</Nav.Link>
-            <Nav.Link className="active" href="/TravelAgency">Travel Agency</Nav.Link>
+          <Nav.Link className="active" href="/IATA">
+            IATA
+          </Nav.Link>
+          <Nav.Link className="active" href="/Airline">
+            Airline
+          </Nav.Link>
+          <Nav.Link className="active" href="/Airport">
+            Airport
+          </Nav.Link>
+          <Nav.Link className="active" href="/TravelAgency">
+            Travel Agency
+          </Nav.Link>
           <Nav className="ml-auto">
-            <Nav.Link className="active" href="/Signup">Sign Up</Nav.Link>
-            <Nav.Link className="active" href="/Login">Log In</Nav.Link>
+            <Nav.Link className="active" href="/Signup">
+              Sign Up
+            </Nav.Link>
+            <Nav.Link className="active" href="/Login">
+              Log In
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
