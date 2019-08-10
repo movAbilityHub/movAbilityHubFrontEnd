868ba81830e6dfd766f33d516585673583d94b71
SkywalkerZ <abraarshk98@gmail.com>
2019-08-10 23:38:36 +0530

    Added open/closed requests for travel agent

--- src/components/travelAgent/closedRequests.js
+++ src/components/travelAgent/closedRequests.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 class ClosedRequests extends Component {
   state = {};
   render() {
-    return <div />;
+    return (
+      <Card className="card" style={{ width: "60%" }}>
+        <Card.Header className="text-center">
+          RequestID No: {/*ID props added here?*/}
+        </Card.Header>
+        <Card.Body>
+          <ListGroup>
+            <ListGroup.Item>Name: </ListGroup.Item>
+            <ListGroup.Item>Disability: </ListGroup.Item>
+            <ListGroup.Item>Ticket No: </ListGroup.Item>
+            <ListGroup.Item>Departure Airport: </ListGroup.Item>
+          </ListGroup>
+          <br />
+          <Card.Text>Status:Closed</Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted text-center">
+          Timestamp comes here!
+        </Card.Footer>
+      </Card>
+    );
   }
 }
 
