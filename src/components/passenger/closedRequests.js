b65c5c81b85a775316659c2146a41a62f80351a3
SkywalkerZ <abraarshk98@gmail.com>
2019-08-10 23:38:04 +0530

    Formatted open/closed requests for passenger

--- src/components/passenger/closedRequests.js
+++ src/components/passenger/closedRequests.js
@@ -6,25 +6,25 @@ class ClosedRequests extends Component {
   state = {};
   render() {
     return (
-              <Card className="card" style={{ width: '60%' }}>
-              <Card.Header className="text-center">RequestID No: {/*ID props added here?*/}</Card.Header>
-              <Card.Body>
-                <ListGroup>
-                <ListGroup.Item>Name: </ListGroup.Item>
-                <ListGroup.Item>Disability: </ListGroup.Item>
-                <ListGroup.Item>Ticket No: </ListGroup.Item>
-                <ListGroup.Item>Departure Airport: </ListGroup.Item>
-                </ListGroup>
-                <br></br>
-                <Card.Text>
-                  Status:Closed
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer className="text-muted text-center">Timestamp comes here!</Card.Footer>
-            </Card>
-      
+      <Card className="card" style={{ width: "60%" }}>
+        <Card.Header className="text-center">
+          RequestID No: {/*ID props added here?*/}
+        </Card.Header>
+        <Card.Body>
+          <ListGroup>
+            <ListGroup.Item>Name: </ListGroup.Item>
+            <ListGroup.Item>Disability: </ListGroup.Item>
+            <ListGroup.Item>Ticket No: </ListGroup.Item>
+            <ListGroup.Item>Departure Airport: </ListGroup.Item>
+          </ListGroup>
+          <br />
+          <Card.Text>Status:Closed</Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted text-center">
+          Timestamp comes here!
+        </Card.Footer>
+      </Card>
     );
-
   }
 }
 
--- src/components/passenger/openRequests.js
+++ src/components/passenger/openRequests.js
@@ -9,26 +9,28 @@ class OpenRequests extends Component {
   state = {};
   render() {
     return (
-        <Card className="card" style={{ width: '60%' }}>
-        <Card.Header className="text-center">RequestID No: {/*ID props added here?*/}</Card.Header>
+      <Card className="card" style={{ width: "60%" }}>
+        <Card.Header className="text-center">
+          RequestID No: {/*ID props added here?*/}
+        </Card.Header>
         <Card.Body>
           <ListGroup>
-          <ListGroup.Item>Name: </ListGroup.Item>
-          <ListGroup.Item>Disability: </ListGroup.Item>
-          <ListGroup.Item>Ticket No: </ListGroup.Item>
-          <ListGroup.Item>Departure Airport: </ListGroup.Item>
+            <ListGroup.Item>Name: </ListGroup.Item>
+            <ListGroup.Item>Disability: </ListGroup.Item>
+            <ListGroup.Item>Ticket No: </ListGroup.Item>
+            <ListGroup.Item>Departure Airport: </ListGroup.Item>
           </ListGroup>
-          <br></br>
-          <Card.Text>
-            Services needed come here!
-          </Card.Text>
-          <Button className="button-center" variant="primary">Cancel Request</Button>
+          <br />
+          <Card.Text>Services needed come here!</Card.Text>
+          <Button className="button-center" variant="primary">
+            Cancel Request
+          </Button>
         </Card.Body>
-        <Card.Footer className="text-muted text-center">Timestamp comes here!</Card.Footer>
+        <Card.Footer className="text-muted text-center">
+          Timestamp comes here!
+        </Card.Footer>
       </Card>
-
     );
-
   }
 }
 
