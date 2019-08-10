c3bdd41d0abf8ba89e2b72e6f39744d7832d4b7d
SkywalkerZ <abraarshk98@gmail.com>
2019-08-10 23:17:27 +0530

    Added Passenger Open Requests

--- src/components/passenger/openRequests.js
+++ src/components/passenger/openRequests.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+
+import "../../assets/styles/tapaOpenRequests.css";
 
 class OpenRequests extends Component {
   state = {};
   render() {
     return (
-    <h1>I HATE THIS BUG</h1>
+        <Card className="card" style={{ width: '60%' }}>
+        <Card.Header className="text-center">RequestID No: {/*ID props added here?*/}</Card.Header>
+        <Card.Body>
+          <ListGroup>
+          <ListGroup.Item>Name: </ListGroup.Item>
+          <ListGroup.Item>Disability: </ListGroup.Item>
+          <ListGroup.Item>Ticket No: </ListGroup.Item>
+          <ListGroup.Item>Departure Airport: </ListGroup.Item>
+          </ListGroup>
+          <br></br>
+          <Card.Text>
+            Services needed come here!
+          </Card.Text>
+          <Button className="button-center" variant="primary">Cancel Request</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted text-center">Timestamp comes here!</Card.Footer>
+      </Card>
 
     );
 
