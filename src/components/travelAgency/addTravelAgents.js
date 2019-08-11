5b387952e84964e0a917f5b829ead78d40af8fdb
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-11 23:35:20 +0530

    Travel agency dashboard and add agent created

---
+++ src/components/travelAgency/addTravelAgents.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+import "../../assets/styles/taDashboard.css";
+
+class AddTravelAgents extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleTimeChange = this.handleTimeChange.bind(this);
+    this.RadioClicked = this.RadioClicked.bind(this);
+
+    this.state = {
+      time: 0,
+      radio: ""
+    };
+  }
+
+  handleTimeChange(time) {
+    console.log(time); // <- prints "3600" if "01:00" is picked
+    this.setState({ time });
+  }
+
+  RadioClicked(e) {
+    console.log("here");
+    this.setState({ [e.target.name]: e.target.value }, function() {
+      console.log(this.state.radio);
+    });
+  }
+
+  render() {
+    return (
+      <Form.Row>
+        <h2 className="text-center col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 mb-5">
+          <i>Add a new Travel Agent</i>
+        </h2>
+
+        <Form.Group
+          as={Col}
+          className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+        >
+          <Form.Label>Agent's First Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="firstName"
+            placeholder="Enter First Name"
+          />
+        </Form.Group>
+
+        <Form.Group
+          as={Col}
+          className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+        >
+          <Form.Label>Agent's Last Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="lastName"
+            placeholder="Enter Last Name"
+          />
+        </Form.Group>
+
+        <Form.Group
+          as={Col}
+          className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+        >
+          <Form.Label>Agent's Phone Number</Form.Label>
+          <Form.Control
+            placeholder="Enter Phone No."
+            namr="phoneNumber"
+            type="number"
+          />
+        </Form.Group>
+
+        <Form.Group
+          as={Col}
+          className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+        >
+          <Form.Label>Agent's Email ID</Form.Label>
+          <Form.Control
+            type="Email"
+            name="email"
+            placeholder="Enter Email ID"
+          />
+        </Form.Group>
+
+        <Form.Group
+          as={Col}
+          className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+        >
+          <Form.Label>Temporary Password</Form.Label>
+          <Form.Control
+            placeholder="Enter Password"
+            name="password"
+            type="password"
+          />
+        </Form.Group>
+
+        <Form.Group
+          as={Col}
+          className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+        >
+          <Form.Label>Retype Temporary Password</Form.Label>
+          <Form.Control
+            placeholder="Enter Password"
+            name="password2"
+            type="password"
+          />
+        </Form.Group>
+
+        <Button type="submit" className="w-25 mt-4 mx-auto">
+          Submit
+        </Button>
+      </Form.Row>
+    );
+  }
+}
+
+export default AddTravelAgents;
---
+++ src/components/travelAgency/dashboard.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+
+import AddtravelAgents from "./addTravelAgents";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+
+import "../../assets/styles/navbar.css";
+
+class Airline extends Component {
+  constructor() {
+    super();
+    document.title = "Dashboard";
+    this.signOut = this.signOut.bind(this);
+  }
+  signOut(e) {
+    localStorage.removeItem("session");
+    sessionStorage.removeItem("session");
+    if (
+      !localStorage.getItem("session") &&
+      !sessionStorage.getItem("session")
+    ) {
+      this.props.history.push("/");
+    }
+  }
+  render() {
+    return (
+      <div className="vh-100">
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          variant="dark"
+          fixed="top"
+          sticky="top"
+          className="navBackground"
+        >
+          <Navbar.Brand href="/">
+            <i className="mov">mov</i>
+            <b className="ability">Ability</b> Hub
+            <sub className="smallFont">by IATA</sub>
+          </Navbar.Brand>
+          <div className="placeHolder" />
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link
+                className="active rounded"
+                as={Link}
+                to="/TravelAgency/Dashboard/"
+              >
+                Add Travel Agent
+              </Nav.Link>
+              <Nav.Link
+                className="active rounded"
+                as={Link}
+                to="/TravelAgency/Dashboard/TravelAgents"
+              >
+                Travel Agents
+              </Nav.Link>
+            </Nav>
+            <Nav className="ml-auto">
+              <Button onClick={this.signOut} className="rounded">
+                Sign Out
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="wrapper">
+          <Switch>
+            <Route
+              exact
+              path="/TravelAgency/Dashboard/"
+              component={AddtravelAgents}
+            />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Airline;
