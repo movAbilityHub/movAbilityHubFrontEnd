8f34aeddbfdfcf78377e7b33c23692d883e3fafe
SkywalkerZ <abraarshk98@gmail.com>
2019-08-12 12:03:20 +0530

    Added CareReceiver Tab functionality

--- src/components/passenger/careReceiver.js
+++ src/components/passenger/careReceiver.js
@@ -2,48 +2,66 @@ import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import 'react-phone-number-input/style.css';
-import PhoneInput from 'react-phone-number-input';
+import PhoneInput from "react-phone-number-input";
 
-import 'react-phone-number-input/style.css';
+import "react-phone-number-input/style.css";
 
 class CareReceiver extends Component {
   state = {};
   render() {
     return (
       <div>
-    <h4>Update Details</h4>
-    <Form>
-    <Form.Row>
-      <Form.Group as={Col} className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4" controlId="formGridOldPassword">
-        <Form.Label>Old Password</Form.Label>
-        <Form.Control type="text" placeholder="Enter Old Password" />
-      </Form.Group>
-      </Form.Row>
-      <Form.Row>
-      <Form.Group as={Col} className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4" controlId="formGridNewPassword">
-        <Form.Label>New Password</Form.Label>
-        <Form.Control type="text" placeholder="Enter New Password" />
-        </Form.Group>
-        <Form.Group as={Col} className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4" controlId="formGridNew2Password">
-        <Form.Label>Re-Enter New Password</Form.Label>
-        <Form.Control type="text" placeholder="Re-Enter New Password" />
-        </Form.Group>
-      </Form.Row>
-      <Form.Row>
-      <Form.Group as={Col} className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4" controlId="formGridNewPhoneNo">
-        <Form.Label>New Phone No.</Form.Label>
-        <PhoneInput
-        placeholder="Enter phone number"
-        value={ this.state.phone }
-        onChange={ phone => this.setState({ phone }) } /> 
-        </Form.Group>
-      </Form.Row>
-      <Button className="updateBtn" type="submit"> Update</Button>
-      </Form>
+        <h4>Update Details</h4>
+        <Form>
+          <Form.Row>
+            <Form.Group
+              as={Col}
+              className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+              controlId="formGridOldPassword"
+            >
+              <Form.Label>Old Password</Form.Label>
+              <Form.Control type="text" placeholder="Enter Old Password" />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group
+              as={Col}
+              className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+              controlId="formGridNewPassword"
+            >
+              <Form.Label>New Password</Form.Label>
+              <Form.Control type="text" placeholder="Enter New Password" />
+            </Form.Group>
+            <Form.Group
+              as={Col}
+              className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+              controlId="formGridNew2Password"
+            >
+              <Form.Label>Re-Enter New Password</Form.Label>
+              <Form.Control type="text" placeholder="Re-Enter New Password" />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group
+              as={Col}
+              className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+              controlId="formGridNewPhoneNo"
+            >
+              <Form.Label>New Phone No.</Form.Label>
+              <PhoneInput
+                placeholder="Enter phone number"
+                value={this.state.phone}
+                onChange={phone => this.setState({ phone })}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Button className="updateBtn" type="submit">
+            {" "}
+            Update
+          </Button>
+        </Form>
       </div>
     );
-
   }
 }
 
--- src/components/travelAgent/careReceiver.js
+++ src/components/travelAgent/careReceiver.js
@@ -1,9 +1,65 @@
 import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import PhoneInput from "react-phone-number-input";
 
 class CareReceiver extends Component {
   state = {};
   render() {
-    return <div />;
+    return (
+      <div>
+        <h4>Update Details</h4>
+        <Form>
+          <Form.Row>
+            <Form.Group
+              as={Col}
+              className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+              controlId="formGridOldPassword"
+            >
+              <Form.Label>Old Password</Form.Label>
+              <Form.Control type="text" placeholder="Enter Old Password" />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group
+              as={Col}
+              className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+              controlId="formGridNewPassword"
+            >
+              <Form.Label>New Password</Form.Label>
+              <Form.Control type="text" placeholder="Enter New Password" />
+            </Form.Group>
+            <Form.Group
+              as={Col}
+              className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+              controlId="formGridNew2Password"
+            >
+              <Form.Label>Re-Enter New Password</Form.Label>
+              <Form.Control type="text" placeholder="Re-Enter New Password" />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group
+              as={Col}
+              className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4"
+              controlId="formGridNewPhoneNo"
+            >
+              <Form.Label>New Phone No.</Form.Label>
+              <PhoneInput
+                placeholder="Enter phone number"
+                value={this.state.phone}
+                onChange={phone => this.setState({ phone })}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Button className="updateBtn" type="submit">
+            {" "}
+            Update
+          </Button>
+        </Form>
+      </div>
+    );
   }
 }
 
