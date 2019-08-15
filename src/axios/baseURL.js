f9f97f74ffb316ce4df06a14fe83766ee7b24fa0
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-15 16:43:05 +0530

    port updated

--- src/axios/baseURL.js
+++ src/axios/baseURL.js
@@ -2,7 +2,7 @@ const getBaseUrl = () => {
   if (process.env.NODE_ENV === "production") {
     return "http://abc.xyz";
   }
-  return "http://localhost:4000";
+  return "http://localhost:5000";
 };
 
 export default getBaseUrl();
