e4e30165cec98f402de4fdb4e321c8f3feda0010
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-15 21:06:08 +0530

    new api calls

--- src/axios/apiCalls.js
+++ src/axios/apiCalls.js
@@ -45,3 +45,63 @@ export const travelAgentLogin = user => {
     method: "POST"
   });
 };
+
+export const customerRegister = user => {
+  return axios({
+    data: {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      phoneNumber: user.phone,
+      password: user.password,
+      password2: user.password1
+    },
+    url: `${BASE_URL}/customer/register`,
+    method: "POST"
+  });
+};
+
+export const staffRegister = user => {
+  return axios({
+    data: {
+      organisationName: user.organisationName,
+      code: user.code,
+      email: user.email,
+      phoneNumber: user.phone,
+      password: user.password,
+      password2: user.password1,
+      userType: user.userType
+    },
+    url: `${BASE_URL}/staffOthers/register`,
+    method: "POST"
+  });
+};
+
+export const storeRegister = request => {
+  return axios({
+    data: {
+      passportNumber: request.passportNumber,
+      ticketNumber: request.ticketNumber,
+      travelTime: request.travelTime,
+      travelDate: request.travelDate,
+      flightNumber: request.flightNumber,
+      origin: request.origin,
+      destination: request.destination,
+      requestedBy: request.requestedBy,
+      requesterID: request.requesterID,
+      disability: request.disability,
+      age: request.age,
+      service: request.service,
+      requestedFor: request.requestedFor,
+      phoneNumber: request.phoneNumber,
+      transitDestination: request.transitDestination,
+      transitDestinationCode: request.transitDestinationCode,
+      destinationCode: request.destinationCode,
+      originCode: request.originCode,
+      airline: request.airline,
+      airlineCode: request.airlineCode
+    },
+    url: `${BASE_URL}/request/storeRequest`,
+    method: "POST"
+  });
+};
