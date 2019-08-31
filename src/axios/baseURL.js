c905d246f5fc7ccf4de948ca3cfa231a15424d31
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-31 22:23:46 +0530

    fix for production again

--- src/axios/baseURL.js
+++ src/axios/baseURL.js
@@ -2,9 +2,10 @@
 const dotenvConfig = require("dotenv").config();
 
 const build = process.env.NODE_ENV;
+console.log("build = " + process.env.NODE_ENV);
 
 const getBaseUrl = () => {
-  if (build === "build") {
+  if (build === "development") {
     return "http://localhost:5000";
   }
   return "https://movability.cleverapps.io";
