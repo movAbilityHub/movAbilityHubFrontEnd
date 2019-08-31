abe7ae928502cd1f5315d8f0656665533d2aeeaa
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-31 16:36:30 +0530

    baseURL for API updated

--- src/axios/baseURL.js
+++ src/axios/baseURL.js
@@ -1,6 +1,8 @@
 const getBaseUrl = () => {
-  return "https://movability.cleverapps.io";
-  //return "http://localhost:5000";
+  if (process.env.NODE_ENV === "production") {
+    return "https://movability.cleverapps.io";
+  }
+  return "http://localhost:5000";
 };
 
 export default getBaseUrl();
