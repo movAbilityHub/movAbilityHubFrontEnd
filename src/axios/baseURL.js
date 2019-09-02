0c6d7ffc749d8ce79cae32e0a72bd82fb6ccf132
Rohith Prakash <rohithprakash99@gmail.com>
2019-09-02 08:28:07 +0530

    fix

--- src/axios/baseURL.js
+++ src/axios/baseURL.js
@@ -1,13 +1,4 @@
-// eslint-disable-next-line
-const dotenvConfig = require("dotenv").config();
-
-const build = process.env.NODE_ENV;
-console.log("build = " + process.env.NODE_ENV);
-
 const getBaseUrl = () => {
-  if (build === "development") {
-    return "http://localhost:5000";
-  }
   return "https://movability.cleverapps.io";
 };
 
